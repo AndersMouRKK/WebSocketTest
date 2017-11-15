9b8b51012a61091293cbffee1d70f46dd9b9e2a4
Unknown <anders.mousten@rokoko.com>
2017-11-15 15:30:08 +0100

    Barebone test

---
+++ WebSocketTest/Assets/Scripts/Client.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+public class Client : MonoBehaviour {
+
+    public string serverIp = "127.0.0.1";
+    public string serverPort = "10000";
+
+    public bool connect = false;
+    public bool ping = false;
+
+    bool connected = false;
+
+    WebSocket webSocket;
+
+	void Start () {
+        connect = false;
+        ping = false;
+        webSocket = new WebSocket("ws://" + serverIp + ":" + serverPort);
+        webSocket.OnOpen += (sender, e) =>
+        {
+            Debug.Log("Socket did open");
+        };
+        webSocket.OnMessage += (sender, e) =>
+        {
+            Debug.Log("Server says: " + e.Data);
+        };
+        webSocket.OnError += (sender, e) =>
+        {
+            Debug.LogWarning("Socket error: " + e.Message);
+        };
+	}
+
+    void Update()
+    {
+        if(connected != connect)
+        {
+            connected = connect;
+            if (connect)
+                webSocket.Connect();
+            else
+                webSocket.Close();
+        }
+        if(connected && ping)
+        {
+            ping = false;
+            webSocket.Send("hello");
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        if (connected)
+            webSocket.Close();
+    }
+}
---
+++ WebSocketTest/Assets/Scripts/Server.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+public class Server : MonoBehaviour {
+
+    public string serverIp = "127.0.0.1";
+    public string serverPort = "10000";
+
+    internal class Ping : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Debug.Log("Client says: " + e.Data);
+            Send("pong");
+        }
+    }
+
+    WebSocketServer server;
+
+	void Start () {
+        server = new WebSocketServer("ws://" + serverIp + ":" + serverPort);
+        server.AddWebSocketService<Ping>("/ping");
+        server.Start();
+        Debug.Log("Server started");
+	}
+
+    void OnApplicationQuit()
+    {
+        server.Stop();
+    }
+}
