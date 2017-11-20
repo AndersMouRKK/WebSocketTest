127d5d553b01d9e3e2227e0de6f082132eede8fe
Unknown <anders.mousten@rokoko.com>
2017-11-20 10:51:19 +0100

    UI changes for mobile testing

--- WebSocketTest/Assets/Scripts/Client.cs
+++ WebSocketTest/Assets/Scripts/Client.cs
@@ -8,9 +8,9 @@ public class Client : MonoBehaviour {
 	public string serverIp = "192.168.3.213";
     public string serverPort = "4000";
 
-    public bool connect = false;
+    /*public bool connect = false;
     public bool ping = false;
-    public bool send100Bytes = false;
+    public bool send100Bytes = false;*/
 	public bool connectAsync = true;
 
     bool connected = false;
@@ -18,18 +18,18 @@ public class Client : MonoBehaviour {
     WebSocket webSocket;
 
 	void Start () {
-        connect = false;
-        ping = false;
+        //connect = false;
+        //ping = false;
         webSocket = new WebSocket("ws://" + serverIp + ":" + serverPort + "/ping");
         webSocket.OnOpen += (sender, e) =>
         {
-			connect = true;
+			//connect = true;
 			connected = true;
             Debug.Log("Socket did open");
         };
 		webSocket.OnClose += (object sender, CloseEventArgs e) => 
 		{
-			connect = false;
+			//connect = false;
 			connected = false;
 			Debug.Log("Socket did close" + e.Reason);
 		};
@@ -52,40 +52,6 @@ public class Client : MonoBehaviour {
 
     void Update()
     {
-        if(connected != connect)
-        {
-			if (connect) {
-				connect = false;
-				Debug.Log ("Trying to connect to server..");
-				if (connectAsync)
-					webSocket.ConnectAsync ();
-				else
-					webSocket.Connect ();
-				Debug.Log ("Finished Connect() / ConnectAsync() call");
-			} else {
-				connect = true;
-				Debug.Log ("Trying to close connection");
-				webSocket.CloseAsync ();
-			}
-        }
-        if(connected)
-        {
-            if (ping)
-            {
-                ping = false;
-                webSocket.Send("hello");
-            }
-            else if (send100Bytes)
-            {
-                send100Bytes = false;
-                byte[] data = new byte[100];
-                for(int i = 0; i < data.Length; i++)
-                {
-                    data[i] = (byte)i;
-                }
-                webSocket.Send(data);
-            }
-        }
     }
 
     void OnApplicationQuit()
@@ -96,5 +62,41 @@ public class Client : MonoBehaviour {
 			else
 				webSocket.Close ();
 		}
+    }
+
+    private void OnGUI()
+    {
+        if (!connected && GUI.Button(new Rect(50, 50, 150, 30), "Connect"))
+        {
+            //connect = false;
+            Debug.Log("Trying to connect to server..");
+            if (connectAsync)
+                webSocket.ConnectAsync();
+            else
+                webSocket.Connect();
+            Debug.Log("Finished Connect() / ConnectAsync() call");
+        }
+        else if (connected && GUI.Button(new Rect(50, 50, 150, 30), "Close"))
+        {
+            //connect = true;
+            Debug.Log("Trying to close connection");
+            webSocket.CloseAsync();
+        }
+        if (connected)
+        {
+            if (GUI.Button(new Rect(50, 100, 150, 30), "Send Text"))
+            {
+                webSocket.Send("hello");
+            }
+            else if (GUI.Button(new Rect(50, 150, 150, 30), "Send Binary Package"))
+            {
+                byte[] data = new byte[100];
+                for (int i = 0; i < data.Length; i++)
+                {
+                    data[i] = (byte)i;
+                }
+                webSocket.Send(data);
+            }
+        }
     }
 }
